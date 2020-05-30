3c723f26d3d12d28bb0949ae4ae312342a269971
burninggun <willchengsters@gmail.com>
2020-05-30 15:36:14 -0700

    sample key.js

---
+++ server/config/keys.js
@@ -0,0 +1,9 @@
+module.exports = {
+    github_strategy: {
+        clientID: null, //insert your github OAUTH ID here
+    },
+    session: {
+        cookieKey: 'asdfasdf' //some random key for authentication,
+    },
+    mongoURI: null, "URI for your mongo database"
+}
