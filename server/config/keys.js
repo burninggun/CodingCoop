d85f07589fef220791cad49f2db7f42f43cfe7d4
burninggun <willchengsters@gmail.com>
2020-05-30 15:44:09 -0700

    removing npm and updating versions

--- server/config/keys.js
+++ server/config/keys.js
@@ -1,9 +1,9 @@
 module.exports = {
     github_strategy: {
-        clientID: null, //insert your github OAUTH ID here
+        clientID: "8fa59b3a6a90a8734cee", //insert your github OAUTH ID here
     },
     session: {
         cookieKey: 'asdfasdf' //some random key for authentication,
     },
-    mongoURI: null, "URI for your mongo database"
+    mongoURI: "mongodb://burninggun:khalid123505@ds015700.mlab.com:15700/coding-coops", //URI for your mongo database
 }
